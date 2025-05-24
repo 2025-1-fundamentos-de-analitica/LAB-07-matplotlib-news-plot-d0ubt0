968adbb396477b4af7a700b1559a2b3a4435d1e3
d0ubt <sepabonn@unal.edu.co>
2025-05-23 22:34:31 -0500

    V1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,6 +4,9 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 # pylint: disable=import-outside-toplevel
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
 def pregunta_01():
     """
@@ -16,3 +19,40 @@ def pregunta_01():
     El gráfico debe salvarse al archivo `files/plots/news.png`.
 
     """
+
+    df = pd.read_csv('files/input/news.csv', index_col= 0)
+
+    colors = {'Television': '#5a5', 'Newspaper': '#7d7', 'Internet': '#f88', 'Radio': '#9d9'}
+    z = {'Television': 1, 'Newspaper': 1, 'Internet': 2, 'Radio': 1}
+    line_width = {'Television': 2, 'Newspaper': 2, 'Internet': 4, 'Radio': 2}
+
+    for columna in df.columns:
+        plt.plot(df[columna], label = columna,  color = colors[columna], zorder = z[columna], linewidth = line_width[columna])
+
+    plt.title('Uso de los medios de comunicacion', fontsize = 16)
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['left'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    #plt.legend(loc= 'upper right')
+    plt.gca().axes.get_yaxis().set_visible(False)
+
+    for col in df.columns:
+        first_year = df.index[0]
+
+        plt.scatter(x = first_year, y = df[col][first_year], color = colors[col], zorder = z[col])
+
+        last_year = df.index[-1]
+
+        plt.scatter(x = last_year, y = df[col][last_year],  color = colors[col], zorder = z[col])
+        
+        plt.text(first_year - 0.2, df[col][first_year], col + " " + str(df[col][first_year]) + '%', ha= 'right', va = 'center', color = colors[col])
+        plt.text(last_year + 0.2, df[col][last_year], str(df[col][last_year]) + '%', ha= 'left', va = 'center', color = colors[col])
+
+
+    if not os.path.exists('files/plots'):
+        os.mkdir('files/plots')
+
+    plt.tight_layout()
+    plt.savefig('files/plots/news.png')
+
+pregunta_01()
